246691abe5160638f641c8b0d423fea5bec1d9bc
zp994643198 <34149653+zp994643198@users.noreply.github.com>
2020-04-16 18:53:01 +0800

    COMMIT

--- jsFunction/jsFunction.js
+++ jsFunction/jsFunction.js
@@ -34,10 +34,10 @@ class myPromise{
         }
     }
     then(onFulfilled, onRejected){
-        console.log('this.state ',this.state)
+        // console.log('this.state ',this.state)
         switch (this.state) {
             case 'fulfilled':
-                console.log('value ',this.value)
+                // console.log('value ',this.value)
                 onFulfilled(this.value)
                 break;
             case 'rejected':
@@ -82,4 +82,63 @@ class myPromise{
 //     }
 // }
 
+Function.prototype.mycall = function(context){
+    ////////判断调用的是否为函数
+    if(typeof this !== 'function'){
+        throw new TypeError('not function')
+    }
+    
+    context = context || window;//判断转换this的目标是否存在,不存在转为window
+    context.fn = this;//函数方法赋值给转换this值后的作用域
 
+    let arg = [...arguments].slice(1);//从第二个参数开始读取
+    let result = context.fn(...arg);//将读取的参数放到新的作用域中的方法中，返回结果
+
+    delete context.fn;
+    return result;
+}
+
+Function.prototype.myapply = function(context){
+    if(typeof this !== 'function'){
+        throw new TypeError('not function')
+    }
+    context = context || window;
+    context.fn = this;
+
+    if(arguments[1]){
+        var arg = [...arguments].slice(1)
+        var result = context.fn(...arg[0]) 
+    }else{
+        var result = context.fn()
+    }
+
+    delete context.fn
+    return result   
+}
+
+Function.prototype.mybind = function(context){
+    if (typeof this !== 'function'){
+        throw new TypeError('Error')
+    }
+
+    let _this = this;
+    let arg = [...arguments].slice(1);
+
+    return function(){
+        
+        return _this.apply(context,arg.concat(...arguments))
+        
+    }
+}
+
+function deepClone(obj){
+    let copy = obj instanceof Array ? [] : {}
+    console.log(copy)
+    for(let i in obj){
+        console.log('obj.hasOwnProperty(i)',obj.hasOwnProperty(i))
+        if(obj.hasOwnProperty(i)){
+            copy[i] = typeof obj[i] === 'object' ? deepClone(obj[i]):obj[i]
+        }
+    }
+    return copy
+}
